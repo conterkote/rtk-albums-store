f6caba92f4d812058c1c5392f2851af786e34728
conterkote <conterkote@mail.ru>
2023-01-18 09:34:12 +0700

    for merge

--- src/store/APIs/albumsAPI.ts
+++ src/store/APIs/albumsAPI.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { IAlbum, IAlbumWithNewValue, IUser } from "../../models";
 import {faker} from "@faker-js/faker";
 
-const commitTest = 'Hello world!'
+const commitTest = 'Hello world haha merge it!'
 
 const albumsAPI = createApi({
   reducerPath: 'albums',
